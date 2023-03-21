6c077ab5024e8925e864f5a1f1b49e881fb200a0
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-03-21 15:22:38 +0500

    add calculator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Слайдер
+
 new Swiper('.hero__slider', {
     slidesPerView: 2,
     spaceBetween: 10,
@@ -17,4 +19,34 @@ new Swiper('.hero__slider', {
             spaceBetween: 8,
         }
     }
-})
+});
+
+
+// Калькулятор
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-square');
+const totalPrice = document.querySelector('.js-total-price');
+const calcResultWrapper = document.querySelector('.calc__result-wrapper');
+
+const tariff = {
+    economy: 550,
+    comfort: 1400,
+    premium: 2700,
+}
+
+calcForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(calcForm.width.value);
+
+    if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+        const square = calcForm.width.value * calcForm.length.value;
+        const price = square * tariff[calcForm.tariff.value];
+
+        calcResultWrapper.style.display = 'block';
+
+        totalSquare.textContent = `${square} кв м`;
+        totalPrice.textContent = `${price} руб`;
+    }
+});
+
