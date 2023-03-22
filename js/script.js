1d1a089581af3bb9d94b4af36c6fdc4b378b4413
Elena Kuznetsova <kuzn.elena@yahoo.com>
2023-03-22 19:57:49 +0500

    add validator

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ const totalSquare = document.querySelector('.js-square');
 const totalPrice = document.querySelector('.js-total-price');
 const calcResultWrapper = document.querySelector('.calc__result-wrapper');
 const btnSubmit = document.querySelector('.js-submit');
+const calcOrder = document.querySelector('.calc__order');
 
 const tariff = {
     economy: 550,
@@ -52,7 +53,8 @@ calcForm.addEventListener('submit', (event) => {
         const square = calcForm.width.value * calcForm.length.value;
         const price = square * tariff[calcForm.tariff.value];
 
-        calcResultWrapper.style.display = 'block';
+        calcResultWrapper.classList.add('calc__result-wrapper_show');
+        calcOrder.classList.add('calc__order_show');
 
         totalSquare.textContent = `${square} кв м`;
         totalPrice.textContent = `${price} руб`;
@@ -129,53 +131,75 @@ const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
     modalElem.addEventListener('click', closeModal);
 };
 
-// const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
-// const buttonElems = document.querySelectorAll(btnOpen);
-// const modalElem = document.querySelector(modal);
-
-// modalElem.style.cssText = `
-//     display: flex;
-//     visibility: hidden;
-//     opacity: 0;
-//     transition: opacity ${time}ms ease-in-out;
-//     `;
-
-// const closeModal = event => {
-//     const target = event.target;
-
-//     if (
-//         target === modalElem ||
-//         (btnClose && target.closest(btnClose)) ||
-//         event.code === 'Escape'
-//         ) {
-        
-//         modalElem.style.opacity = 0;
-
-//     setTimeout(() => {
-//         modalElem.style.visibility = 'hidden';
-//         }, time);
-
-//         window.removeEventListener('keydown', closeModal);
-//     }
-// }
-
-// const openModal = () => {
-//     modalElem.style.visibility = 'visible';
-//     modalElem.style.opacity = 1;
-//     window.addEventListener('keydown', closeModal)
-//     };
-
-//     buttonElems.forEach(btn => {
-//         btn.addEventListener('click', openModal);
-//     });
-
-//     modalElem.addEventListener('click', closeModal);
-// };
-
-
 modalController({
     modal: '.modal',
     btnOpen: '.js-order',
     btnClose: '.modal__close',
+});
+
+
+// Маска
+
+const phone = document.getElementById('phone');
+const imPhone = new Inputmask('+7(999)999-99-99');
+
+imPhone.mask(phone);
+
+
+
+// Валидатор
+const validator = new JustValidate('.modal__form', {
+    errorLabelCssClass: 'modal__input-error',
+    errorLabelStyle: {
+        color: '#FFC700',
+    }
+});
+
+validator
+    .addField('#name', [
+        {
+            rule: 'required',
+            errorMessage: 'Как вас зовут?',
+        }, 
+        {
+            rule: 'minLength',
+            value: 3,
+            errorMessage: 'Не короче 3-х символов',
+        }
+
+    ])
+    .addField('#phone', [
+        {
+            rule: 'required',
+            errorMessage: 'Укажите ваш телефон',
+        },
+        {
+            validator: value => {
+                const number = phone.inputmask.unmaskedvalue()
+                return number.length === 10
+            },
+            errorMessage: 'Телефон не корректный',
+            
+        }
+    ]);
+
+validator.onSuccess((event) => {
+    const form = event.currentTarget
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+method: 'POST',
+body: JSON.stringify({
+    name: form.name.value,
+    phone: form.phone.value
+}),
+headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+    .then((response) => response.json())
+    .then((data) => {
+        form.reset();
+        alert('Спасибо, ваша заявка принята')
+    });
 })
 
